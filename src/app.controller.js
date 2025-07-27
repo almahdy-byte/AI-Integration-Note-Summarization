c8edde29964a8eadf885e41be1d00b9a32d8205b
MohamedAlmahdy <mohamedalmahdy02@gmail.com>
2025-07-27 21:32:37 +0300

    finish project

--- src/app.controller.js
+++ src/app.controller.js
@@ -4,8 +4,30 @@ import authRouter from "./modules/authModule/auth.router.js";
 import notesRouter from "./modules/notesModule/notes.router.js";
 import { createHandler } from "graphql-http/lib/use/express";
 import { schema } from "./graphql.js";
+import cors from 'cors'
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
 
 export const bootstrap =async(app , express)  => {
+
+    app.use(cors({
+    origin:'*'
+    }))
+
+    app.use(rateLimit({
+        limit : 3,
+        message : 'to many requests , please try again later',
+        skipSuccessfulRequests : true,
+        handler:(req , res , next , options)=>{
+            return next(new Error(options.message , {cause : StatusCodes.TOO_MANY_REQUESTS}))
+        }
+
+    }))
+
+    app.use(helmet({
+        xPoweredBy:false
+    }))
+
 
     app.use(express.json());
     app.use('/uploads' , express.static('uploads'))
--- src/utils/upload/multer.js
+++ src/utils/upload/multer.js
@@ -2,17 +2,10 @@ import { StatusCodes } from "http-status-codes";
 import multer from "multer";
 import { nanoid } from "nanoid";
 
-/**
- * @function uploadFile
- * @description Creates and configures a multer middleware for file uploads
- * @returns {Object} Configured multer middleware for handling file uploads
- */
+
 export const uploadFile = () =>{
 
-    /**
-     * Configure disk storage for uploaded files
-     * @type {Object}
-     */
+
     const storage = multer.diskStorage({
         
         destination : 'uploads',
@@ -25,10 +18,6 @@ export const uploadFile = () =>{
 
     
     
-    /**
-     * Create multer instance with configured storage
-     * @type {Object}
-     */
     const upload = multer({storage});
     return upload
 }
